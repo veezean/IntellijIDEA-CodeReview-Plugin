c94ae5f68f202e839b16eb1703c4d08763d804bf
Veezean <veezean@hotmail.com>
2023-08-13 20:25:11 +0800

    bug fix & support date picker

---
+++ src/com/veezean/idea/plugin/codereviewer/action/element/DateSelectCreator.java
@@ -0,0 +1,23 @@
+package com.veezean.idea.plugin.codereviewer.action.element;
+
+import com.intellij.openapi.ui.ComboBox;
+import com.veezean.idea.plugin.codereviewer.model.Column;
+import com.veezean.idea.plugin.codereviewer.model.ValuePair;
+
+import javax.swing.*;
+
+/**
+ * 日期时间选择组件构造器
+ *
+ * @author Veezean
+ * @since 2023/2/4
+ */
+public class DateSelectCreator implements IElementCreator {
+
+    @Override
+    public JComponent create(Column column, boolean editable) {
+        JTextField jTextField = new JTextField("单击选择日期");
+        new DatePicker().register(jTextField);
+        return jTextField;
+    }
+}
